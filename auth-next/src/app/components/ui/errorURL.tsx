b5575d595c6a9bdff4b4544a2083fcc9fbb2dcb9
egrizq <rizq.ramadhan17@gmail.com>
2024-06-22 21:03:07 +0700

    deleted

--- auth-next/src/app/components/ui/errorURL.tsx
+++
@@ -1,9 +0,0 @@
-export default function ErrorLink() {
-    return (
-        <>
-            <p className='text-red-700 border border-red-700 py-1 px-2 rounded-md'>
-                Pastikan link sesuai dengan Sosial Media-nya 🚨
-            </p>
-        </>
-    )
-}
