e80718e598daca2cfd5ba25bbaac07c444192f26
egrizq <rizq.ramadhan17@gmail.com>
2024-06-24 08:58:27 +0700

    feat: auth into one file

---
+++ auth-go/controller/auth.go
@@ -0,0 +1,98 @@
+package controller
+
+import (
+	"auth-go/database"
+	"auth-go/helpers"
+	"auth-go/model"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Login(ctx *gin.Context) {
+	// bind json
+	user, err := helpers.BindJSON(ctx)
+	if err != nil {
+		model.Response(ctx, http.StatusInternalServerError, "invalid request")
+		return
+	}
+
+	// check username is exist or not
+	userPasswordFromDB, err := helpers.IsUsernameExistForLogin(user.Username)
+	if err != nil {
+		model.Response(ctx, http.StatusUnauthorized, "username or password is invalid")
+		return
+	}
+
+	// compare password
+	err = bcrypt.CompareHashAndPassword([]byte(userPasswordFromDB), []byte(user.Password))
+	if err != nil {
+		model.Response(ctx, http.StatusUnauthorized, "username or password is invalid")
+		return
+	}
+
+	// set session
+	if err := helpers.SetSession(ctx, user.Username); err != nil {
+		model.Response(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// return 200
+	model.Response(ctx, http.StatusOK, user.Username)
+	log.Println("login success")
+}
+
+func SignUp(ctx *gin.Context) {
+	// bind json
+	user, err := helpers.BindJSON(ctx)
+	if err != nil {
+		model.Response(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// check username if exist or not
+	if err = helpers.IsUsernameExistForSignUp(user.Username); err != nil {
+		model.Response(ctx, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	// hash password
+	hashPass, _ := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+	user.Password = string(hashPass)
+
+	// insert to database
+	saveUser := model.FormatUser(user)
+	if result := database.DB.Create(saveUser); result.Error != nil {
+		model.Response(ctx, http.StatusInternalServerError, "an error occurred while creating your account")
+		return
+	}
+
+	// set session
+	if err := helpers.SetSession(ctx, user.Username); err != nil {
+		model.Response(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// return 201
+	model.Response(ctx, http.StatusCreated, "success")
+}
+
+func Logout(ctx *gin.Context) {
+	// delete session
+	session, err := helpers.STORE.Get(ctx.Request, "session")
+	if err != nil {
+		model.Response(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+	session.Options.MaxAge = -1
+
+	if err := session.Save(ctx.Request, ctx.Writer); err != nil {
+		model.Response(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// response 200
+	model.Response(ctx, http.StatusOK, "you're logout")
+}
