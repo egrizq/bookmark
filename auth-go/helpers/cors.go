df26774f8708cf823dadb887c437c3e4f43ce3f2
egrizq <rizq.ramadhan17@gmail.com>
2024-06-30 16:14:56 +0700

    feat: delete method

--- auth-go/helpers/cors.go
+++ auth-go/helpers/cors.go
@@ -9,7 +9,7 @@ func CORS() gin.HandlerFunc {
 			"http://localhost:3000") // only for FE
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
